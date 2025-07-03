75b5ebf3bae6a6077bd6be16beeb66e3e9f46350
aureliadw <aureliadwiyanputri@gmail.com>
2025-07-03 12:01:02 +0700

    Update operator.php

--- operator.php
+++ operator.php
@@ -1,5 +1,4 @@
-<!--aritmatika*// -->
-
+<!-- Aritmatika -->
 <?php
 $a = 10;
 $b = 3;
@@ -11,17 +10,14 @@ echo "Hasil a / b = " . ($a / $b) . "\n";
 echo "Sisa bagi a % b = " . ($a % $b) . "\n";
 ?>
 
-
-<!--penugasan*//
+<!-- Penugasan -->
 <?php
 $a = 5;
 $a += 3;
-echo "Nilai a sekarang: $a\n"; // 8
-
+echo "Nilai a sekarang: $a\n"; // Output: 8
 ?>
 
-
-//perbandingan
+<!-- Perbandingan -->
 <?php
 $a = 7;
 $b = 10;
@@ -30,8 +26,7 @@ echo "Apakah a == b? " . ($a == $b ? "Ya" : "Tidak") . "\n";
 echo "Apakah a < b? " . ($a < $b ? "Ya" : "Tidak") . "\n";
 ?>
 
-
-//logika
+<!-- Logika -->
 <?php
 $a = 8;
 $b = 3;
@@ -40,11 +35,9 @@ echo "a > 5 dan b < 5? " . ($a > 5 && $b < 5 ? "Ya" : "Tidak") . "\n";
 echo "a < 5 atau b < 5? " . ($a < 5 || $b < 5 ? "Ya" : "Tidak") . "\n";
 ?>
 
-
-
-//increment,decrement
+<!-- Increment dan Decrement -->
 <?php
 $a = 5;
-echo "Nilai a: " . $a++ . "\n"; // tampil 5, lalu jadi 6
-echo "Setelah ++, a = $a\n";   // tampil 6
+echo "Nilai a: " . $a++ . "\n";       // Tampil 5, lalu a jadi 6
+echo "Setelah ++, a = $a\n";          // Tampil 6
 ?>
